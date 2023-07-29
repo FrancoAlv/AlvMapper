02e0b1b2dbb7a6d17ca3b5e9b349450c2e01967b
franco.caralv <franco.caralv@gmail.com>
2023-07-29 18:44:55 -0500

    procesador mapper

---
+++ Mapper/build.gradle.kts
@@ -0,0 +1,28 @@
+
+plugins {
+    id("java-library")
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+addProcesador()
+
+dependencies {
+    implementation (fileTree("dir" to "libs" ,"include" to "[*.jar]"))
+    compileOnly(project(":MapperDecorador"))
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+fun Project.addProcesador(){
+    dependencies {
+        add("implementation","com.squareup:kotlinpoet:1.11.0")
+        add("implementation","com.google.auto.service:auto-service:1.0-rc4")
+        add("kapt", "com.google.auto.service:auto-service:1.0-rc4")
+    }
+}
